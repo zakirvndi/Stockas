36bf7a53cc6a0f985dba9f8e57aac59d539e729a
christinesilalahi <christinesilalahi12@gmail.com>
2025-04-03 14:53:40 +0700

    Update auth

---
+++ Stockas/Application/Middleware/TokenBlacklistMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Stockas.Application.Services.Token;
+
+namespace Stockas.Application.Middleware
+{
+    public class TokenBlacklistMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ITokenBlacklistService _tokenBlacklistService;
+
+        public TokenBlacklistMiddleware(
+            RequestDelegate next,
+            ITokenBlacklistService tokenBlacklistService)
+        {
+            _next = next;
+            _tokenBlacklistService = tokenBlacklistService;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Headers["Authorization"]
+                .FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null && await _tokenBlacklistService.IsTokenBlacklistedAsync(token))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Token revoked");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
