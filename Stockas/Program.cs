3043ee4b38eb5d305749c6e39ac8b0bc423fbe1b
zakirvndi <revandizaki183@gmail.com>
2025-04-07 08:26:18 +0700

    CORS update

--- Stockas/Program.cs
+++ Stockas/Program.cs
@@ -41,12 +41,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:3000") 
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
+        policy => policy
+            .WithOrigins(
+                "http://localhost:3000"
+            )
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
 });
 
 
